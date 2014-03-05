340195f736db43ec4dd0eb518679b6a4c530cd55
Maxaman33 <maxaman33@gmail.com>
2014-03-04 19:26:44 -0800

    I added a main package thing. So thats nice.

---
+++ src/edu/iastate/cs362/hb/main/Cardinal.java
@@ -0,0 +1,19 @@
+package edu.iastate.cs362.hb.main;
+
+public class Cardinal {
+
+	public static void main(String[] args){
+		new Cardinal().run();
+		System.out.println("Thanks for using our software!");
+	}
+	
+	/**
+	 * This method takes no arguments.
+	 * The method continuously runs and gets String commands.
+	 */
+	private void run(){
+		while(true){
+			//Magic happens here.
+		}
+	}
+}
