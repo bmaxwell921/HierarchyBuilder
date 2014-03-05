32f62b8338f65abc1f38cfdeb55a0e9ed7786a16
Maxaman33 <maxaman33@gmail.com>
2014-03-04 20:02:35 -0800

    Added a few lines to Cardinal.

--- src/edu/iastate/cs362/hb/main/Cardinal.java
+++ src/edu/iastate/cs362/hb/main/Cardinal.java
@@ -1,7 +1,13 @@
 package edu.iastate.cs362.hb.main;
 
+import java.util.Scanner;
+
+import edu.iastate.cs362.hb.commands.ICommandParser;
+
 public class Cardinal {
 
+	private ICommandParser commander;
+	
 	public static void main(String[] args){
 		new Cardinal().run();
 		System.out.println("Thanks for using our software!");
@@ -12,7 +18,10 @@ public class Cardinal {
 	 * The method continuously runs and gets String commands.
 	 */
 	private void run(){
+		Scanner in = new Scanner(System.in);
 		while(true){
+			String command = in.nextLine();
+			commander.parseCommand(command);
 			//Magic happens here.
 		}
 	}
