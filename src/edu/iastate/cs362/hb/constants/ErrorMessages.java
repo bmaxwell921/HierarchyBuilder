2a1e1df315d913655bc2a8f1d55372600eaa6bf6
Brandon <bmaxwell921@gmail.com>
2014-03-05 19:37:45 -0600

    New constants file for error messages

---
+++ src/edu/iastate/cs362/hb/constants/ErrorMessages.java
@@ -0,0 +1,13 @@
+package edu.iastate.cs362.hb.constants;
+
+/**
+ * Class holding onto the error messages found in the
+ * Hierarchy Builder project
+ * @author Brandon
+ *
+ */
+public class ErrorMessages {
+
+	public static final String OBJECT_NOT_FOUND = "Couldn't find the requested class: %s";
+
+}
