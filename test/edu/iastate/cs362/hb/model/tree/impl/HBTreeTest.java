19424aff6b565dd921a5d9accf759cef5e9f71d7
Brandon <bmaxwell921@gmail.com>
2014-03-06 21:58:53 -0600

    Added folder for tests

---
+++ test/edu/iastate/cs362/hb/model/tree/impl/HBTreeTest.java
@@ -0,0 +1,14 @@
+package edu.iastate.cs362.hb.model.tree.impl;
+
+/**
+ * Tests for the HBTree class
+ * @author Brandon
+ *
+ */
+public class HBTreeTest {
+
+	public HBTreeTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
