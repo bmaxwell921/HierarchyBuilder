102568416e9a06b44a9bbd8a86f08e5107188af6
Brandon <bmaxwell921@gmail.com>
2014-03-07 16:14:38 -0600

    Added stuff to the command hierarchy and implemented it

--- src/edu/iastate/cs362/hb/commands/Command.java
+++ src/edu/iastate/cs362/hb/commands/Command.java
@@ -5,35 +5,67 @@ import java.util.Map;
 
 /**
  * Created command with name and value
- * @author rclabough
- *
+ * 
+ * @author rclabough, Brandon
+ * 
  */
 public class Command implements ICommand {
 
+	// The main name of the command
 	private String name;
+
+	// The sub name of the command
+	private String subCmd;
+
+	// Mapping of flags to their values
 	private Map<String, String> value;
-	
-	public Command(String name)
-	{
+
+	public Command(String name) {
 		this.name = name;
 		this.value = new HashMap<String, String>();
 	}
-	
-	public void AddArgument(String name, String value)
-	{
+
+	public void AddArgument(String name, String value) {
 		this.value.put(name, value);
 	}
-	
+
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 	@Override
-	public void setCommand(String line) {
-		// TODO Auto-generated method stub
+	public boolean hasName(String name) {
+		return this.name != null && this.name.equals(name);
+	}
 
+	@Override
+	public String getSubCommand() {
+		return subCmd;
+	}
+
+	@Override
+	public void setSubCommand(String subCommand) {
+		this.subCmd = subCommand;
+	}
+
+	@Override
+	public boolean hasFlag(String flagName) {
+		return value.containsKey(flagName);
+	}
+
+	@Override
+	public String getFlagValue(String flagName) {
+		return value.get(flagName);
+	}
+
+	@Override
+	public boolean addFlagValue(String flagName, String flagValue) {
+		if (value.containsKey(flagName)) {
+			return false;
+		}
+		value.put(flagName, flagValue);
+		return true;
 	}
 
 }
