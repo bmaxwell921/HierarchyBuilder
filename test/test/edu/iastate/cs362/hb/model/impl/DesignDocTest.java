be561ded34b99366cf840304f0bbc401c18755e7
Brandon <bmaxwell921@gmail.com>
2014-03-07 17:30:01 -0600

    Changed to just testing the design doc, everything else just passes along the method calls

--- test/test/edu/iastate/cs362/hb/model/impl/DesignDocTest.java
+++ test/test/edu/iastate/cs362/hb/model/impl/DesignDocTest.java
@@ -5,10 +5,10 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import edu.iastate.cs362.hb.controller.ISystemController;
-import edu.iastate.cs362.hb.controller.impl.SystemController;
 import edu.iastate.cs362.hb.exceptions.HBDuplicateObjectFoundException;
+import edu.iastate.cs362.hb.model.IDesignDoc;
 import edu.iastate.cs362.hb.model.ISystem;
+import edu.iastate.cs362.hb.model.impl.DesignDoc;
 import edu.iastate.cs362.hb.model.impl.HBSystem;
 
 /**
@@ -16,47 +16,47 @@ import edu.iastate.cs362.hb.model.impl.HBSystem;
  * @author Brandon
  *
  */
-public class HBSystemTest {
-
+public class DesignDocTest {
 	private static final String TEST_NAME = "Name";
 	
+	private static final String OBJECT_NAME = "class";
+	
 	// Object to test
-	private ISystem test;
+	private IDesignDoc test;
 	
 	@Before
 	public void setUp() {
-		test = new HBSystem();
-		test.createDesign(TEST_NAME);
+		test = new DesignDoc(TEST_NAME);
 	}
 	
 	@Test
 	public void testCreateDesign() {
-		// Using local test here because test has already called the method
-		ISystem localTest = new HBSystem();
-		Assert.assertTrue("Creating a design should return true.", localTest.createDesign(TEST_NAME));
+		// Should just execute without fail
+		IDesignDoc doc = new DesignDoc(TEST_NAME);
+		Assert.assertEquals("Newly created design doc should have the correct name", TEST_NAME, doc.getName());
 	}
 	
 	@Test
 	public void testCreateClass() throws HBDuplicateObjectFoundException {
-		Assert.assertTrue("Creating a class normally should work", test.createClass(TEST_NAME));
+		Assert.assertTrue("Creating a class normally should work", test.createClass(OBJECT_NAME));
 	}
 	
 	@Test (expected = HBDuplicateObjectFoundException.class)
 	public void testCreateClassDup() throws HBDuplicateObjectFoundException {
-		test.createClass(TEST_NAME);
-		test.createClass(TEST_NAME);
+		test.createClass(OBJECT_NAME);
+		test.createClass(OBJECT_NAME);
 		Assert.fail("Adding two classes with the name name (and default package) should fail");
 	}
 	
 	@Test
 	public void testCreateInterface() throws HBDuplicateObjectFoundException {
-		Assert.assertTrue("Creating an interface normally should work", test.createInterface(TEST_NAME));
+		Assert.assertTrue("Creating an interface normally should work", test.createInterface(OBJECT_NAME));
 	}
 	
 	@Test (expected = HBDuplicateObjectFoundException.class)
 	public void testCreateInterfaceDup() throws HBDuplicateObjectFoundException {
-		test.createInterface(TEST_NAME);
-		test.createInterface(TEST_NAME);
+		test.createInterface(OBJECT_NAME);
+		test.createInterface(OBJECT_NAME);
 		Assert.fail("Adding two classes with the name name (and default package) should fail");
 	}
 	
