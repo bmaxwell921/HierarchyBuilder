e582e3c02230ddc2babb2a110198da1caabbb704
Brandon <bmaxwell921@gmail.com>
2014-03-07 18:23:34 -0600

    Added equals and hashcode

--- src/edu/iastate/cs362/hb/model/impl/HBMethod.java
+++ src/edu/iastate/cs362/hb/model/impl/HBMethod.java
@@ -82,4 +82,37 @@ public class HBMethod implements IMethod {
 		return args;
 	}
 
+	// Equality and Hashes only depend on the name and arguments
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((args == null) ? 0 : args.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HBMethod other = (HBMethod) obj;
+		if (args == null) {
+			if (other.args != null)
+				return false;
+		} else if (!args.equals(other.args))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	
 }
