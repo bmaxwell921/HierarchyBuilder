6416c60c70b0436f9e896f3e466b3f7ce272842b
Rob <rmcn96@gmail.com>
2014-03-10 13:10:45 -0500

    Added support for CmdConstants, exceptions

--- src/edu/iastate/cs362/hb/commands/CommandParser.java
+++ src/edu/iastate/cs362/hb/commands/CommandParser.java
@@ -27,17 +27,9 @@ public class CommandParser implements ICommandParser{
 	public ICommand parseCommand(String command) throws MalformattedCommandException {
 		
 		String[] commandList = command.split(" ");
-		Boolean[] flagType = new Boolean[commandList.length];
 		for(int i = 0; i < commandList.length; i++)
 		{
-			if(commandList[i].charAt(0) == '-')
-			{
-				flagType[i] = true;
-			}
-			else
-			{
-				flagType[i] = false;
-			}
+
 			//Force all toupper.
 			commandList[i] = commandList[i].toLowerCase();
 			
@@ -46,7 +38,7 @@ public class CommandParser implements ICommandParser{
 
 		
 		Command c;
-		switch(commandList[0].substring(1))
+		switch(commandList[0])
 		{
 		case CmdConstants.CmdNames.ADD:
 			c = new Command(CmdConstants.CmdNames.ADD);
@@ -61,42 +53,22 @@ public class CommandParser implements ICommandParser{
 			throw new MalformattedCommandException("Malformatted command name!", commandList[0]);	
 		}
 		
-		
-		for(int x = 1; x < commandList.length; x++)
+		for(int i = 1; i < commandList.length; i++)
 		{
-			switch(commandList[x].substring(1))
+			if(commandList[i].charAt(0) == '-')
 			{
-			case CmdConstants.SubCmdNames.CLASS_REGEX:
-				
-				break;
-			case CmdConstants.SubCmdNames.DESIGN_REGEX:
-				
-				break;
-			case CmdConstants.SubCmdNames.INTERFACE_REGEX:
-				
-				break;
-			case CmdConstants.SubCmdNames.METHOD_REGEX:
-				
-				break;
-			case CmdConstants.SubCmdNames.PACKAGE_REGEX:
-				break;
-			
-			}
-			/**
-			//Check to see if there is a next argument
-			//If the next string is not null, and it's flagtype is false, then it is a value
-			if((commandList[x + 1] != null)&&((flagType[x + 1] != null)&&(flagType[x+1] == false)))
-			{
-				
-				c.addFlagValue(commandList[x].substring(1), commandList[x+1]);
-				x++;
+				//Flag
+				boolean valueUsed = addFlag(c, commandList[i].substring(1), commandList[i+1]);
+				if(valueUsed)
+				{
+					i++;
+				}
 			}
 			else
 			{
-				// TODO same as above
-				c.addFlagValue(commandList[x].substring(1), null);
+				//SubCommand
+				addSubCommand(c, commandList[i]);
 			}
-			**/
 		}
 		
 		
@@ -117,4 +89,66 @@ public class CommandParser implements ICommandParser{
 		
 	}
 
+	public boolean addFlag(Command c, String input, String value)
+	{
+		boolean valueUsed = false;
+		switch(input)
+		{
+		case CmdConstants.Flags.CONTAINER_NAME:
+			c.addFlagValue(CmdConstants.Flags.CONTAINER_NAME, value);
+			valueUsed = true;
+			break;
+		case CmdConstants.Flags.HELP:
+
+			c.addFlagValue(CmdConstants.Flags.HELP, null);
+			break;
+		case CmdConstants.Flags.INSTANCE:
+
+			c.addFlagValue(CmdConstants.Flags.INSTANCE, null);
+			break;
+		case CmdConstants.Flags.NAME:
+
+			c.addFlagValue(CmdConstants.Flags.NAME, value);
+			valueUsed = true;
+			break;
+		case CmdConstants.Flags.PARAMETERS:
+
+			c.addFlagValue(CmdConstants.Flags.PARAMETERS, value);
+			valueUsed = true;
+			break;
+		case CmdConstants.Flags.STATIC:
+
+			c.addFlagValue(CmdConstants.Flags.STATIC, null);
+			break;
+		}
+		
+		return valueUsed;
+	}
+	
+	public void addSubCommand(Command c, String input)
+	{
+
+			switch(input)
+			{
+			case CmdConstants.SubCmdNames.CLASS_REGEX:
+				c.setSubCommand(CmdConstants.SubCmdNames.CLASS_REGEX);
+				break;
+			case CmdConstants.SubCmdNames.DESIGN_REGEX:
+				c.setSubCommand(CmdConstants.SubCmdNames.DESIGN_REGEX);
+				break;
+			case CmdConstants.SubCmdNames.INTERFACE_REGEX:
+				c.setSubCommand(CmdConstants.SubCmdNames.INTERFACE_REGEX);
+				break;
+			case CmdConstants.SubCmdNames.METHOD_REGEX:
+				c.setSubCommand(CmdConstants.SubCmdNames.METHOD_REGEX);
+				break;
+			case CmdConstants.SubCmdNames.PACKAGE_REGEX:
+				c.setSubCommand(CmdConstants.SubCmdNames.PACKAGE_REGEX);
+				break;
+			default:
+				break;
+			}
+			
+		
+	}
 }
