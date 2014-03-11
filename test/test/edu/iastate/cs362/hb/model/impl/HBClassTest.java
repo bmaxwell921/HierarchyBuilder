9d1949f18173a4e2a73b14d214a7e7c1cddd7dcd
Maxaman33 <maxaman33@gmail.com>
2014-03-10 22:58:15 -0700

    Added new Test class.

---
+++ test/test/edu/iastate/cs362/hb/model/impl/HBClassTest.java
@@ -0,0 +1,41 @@
+package test.edu.iastate.cs362.hb.model.impl;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.iastate.cs362.hb.model.IClass;
+import edu.iastate.cs362.hb.model.impl.HBClass;
+
+public class HBClassTest {
+	
+	private String NAME = "NAME";
+	
+	private IClass test;
+	
+	@Before
+	public void setUp(){
+		test = new HBClass(NAME);
+	}
+	
+	@Test
+	public void testRemovePackage(){
+		String packageName = "PAKEAGE";
+		test.addPackage(packageName);
+		test.removePackage();
+		Assert.assertTrue("Normal remove Package.", !(packageName.equals(test.getPackage())));
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	@After
+	public void tearDown(){
+		test = null;
+	}
+}
