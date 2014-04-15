379315a8dc1f70ae7368f843351ade4ced9798cb
Brandon <bmaxwell921@gmail.com>
2014-04-15 00:13:34 -0500

    Added some prototyping for source importing

--- src/edu/iastate/cs362/hb/importer/impl/SourceImporter.java
+++ src/edu/iastate/cs362/hb/importer/impl/SourceImporter.java
@@ -1,8 +1,24 @@
 package edu.iastate.cs362.hb.importer.impl;
 
+import javax.tools.JavaCompiler;
+import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+
 import edu.iastate.cs362.hb.importer.IImporter;
 import edu.iastate.cs362.hb.model.IDesignDoc;
 
+/**
+ * Importer used to turn source code into a DesignDoc. Turns out there's built
+ * in support for this since Java 6 O.O
+ * 
+ * https://today.java.net/pub/a/today/2008/04/10/source-code-analysis-using-java
+ * -6-compiler-apis.html
+ * 
+ * @author Brandon
+ * 
+ */
 public class SourceImporter implements IImporter {
 
 	public SourceImporter() {
@@ -11,8 +27,23 @@ public class SourceImporter implements IImporter {
 
 	@Override
 	public IDesignDoc doImport(String path) {
-		// TODO Auto-generated method stub
+		// Compiler instance
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
+		StandardJavaFileManager fileManager = compiler.getStandardFileManager(
+				null, null, null);
+
+		Iterable<? extends JavaFileObject> compilationUnits = fileManager
+				.getJavaFileObjects("./src/edu/iastate/cs362/hb/importer/impl/SourceImporter.java");
+		
+		CompilationTask task = compiler.getTask(null, fileManager, null, null, null, compilationUnits);
+		task.call();
+		
 		return null;
+	}
+	
+	public static void main(String[] args) {
+		new SourceImporter().doImport("");
 	}
 
 }
