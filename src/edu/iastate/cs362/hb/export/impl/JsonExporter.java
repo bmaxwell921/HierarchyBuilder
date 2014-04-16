3ae7999b0ba0eb1a1181509aa155453ca82fcb69
Brandon <bmaxwell921@gmail.com>
2014-04-15 20:31:34 -0500

    Finished the json exporter

--- src/edu/iastate/cs362/hb/export/impl/JsonExporter.java
+++ src/edu/iastate/cs362/hb/export/impl/JsonExporter.java
@@ -1,34 +1,46 @@
 package edu.iastate.cs362.hb.export.impl;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Set;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import edu.iastate.cs362.hb.export.IExporter;
+import edu.iastate.cs362.hb.io.DesignDocBox;
 import edu.iastate.cs362.hb.model.IDesignDoc;
 import edu.iastate.cs362.hb.model.IObject;
 import edu.iastate.cs362.hb.model.IRelationship;
 import edu.iastate.cs362.hb.model.tree.IHBTreeVisitor;
 import edu.iastate.cs362.hb.model.tree.Pair;
 
-import com.google.gson.Gson;
-
 public class JsonExporter implements IExporter {
 
 	@Override
 	public boolean doExport(String path, IDesignDoc doc) {
-		
-		final Gson gson = new Gson();
-		final StringBuilder sb = new StringBuilder("");
-		doc.traverse(new IHBTreeVisitor(){
+		final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		final DesignDocBox ddBox = new DesignDocBox();
+		ddBox.addName(doc.getName());
+		// Get all of the classes and interfaces
+		doc.traverse(new IHBTreeVisitor() {
 			@Override
 			public void visit(IObject o, Set<Pair<IRelationship, IObject>> superTypes) {
-				sb.append(gson.toJson(o.getName()));
-
+				ddBox.add(o);
 			}
 		});
-		System.out.println(sb.toString());
-		
-		
-		return false;
+		File out = new File(path);
+		// Create things as needed
+		if (!out.exists()) {
+			System.out.println(out.getParentFile().mkdirs());
+		}
+		try (FileWriter fw = new FileWriter(out)) {
+			fw.write(gson.toJson(ddBox));
+		} catch (Exception e) {
+			// TODO something more sophisticated
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
-
 }
