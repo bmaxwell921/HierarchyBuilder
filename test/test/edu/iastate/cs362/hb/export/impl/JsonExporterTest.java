30eabcb2848a508c6504aec9bc20a4f7559108d8
Brandon <bmaxwell921@gmail.com>
2014-04-17 16:58:22 -0500

    Worked on some testing utils...shelved for now

--- test/test/edu/iastate/cs362/hb/export/impl/JsonExporterTest.java
+++ test/test/edu/iastate/cs362/hb/export/impl/JsonExporterTest.java
@@ -1,15 +1,20 @@
 package test.edu.iastate.cs362.hb.export.impl;
 
+import java.io.File;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import test.edu.iastate.cs362.hb.util.ExportUtil;
 import edu.iastate.cs362.hb.export.impl.JsonExporter;
 import edu.iastate.cs362.hb.model.IDesignDoc;
 import edu.iastate.cs362.hb.model.impl.HBDesignDoc;
 
 /**
  * Class used to test to JsonExporter
+ * 
  * @author Brandon
  *
  */
@@ -17,20 +22,36 @@ public class JsonExporterTest {
 
 	// Test vars
 	public static final String DESIGN_NAME = "Design";
-	
+	public static final String FILE_PATH = "." + File.separatorChar + "test"
+			+ File.separatorChar + "exports" + File.separatorChar + "json";
+
+	public static int fileNum = 0;
+
 	private JsonExporter test;
-	
+
 	@Before
 	public void startUp() {
 		test = new JsonExporter();
 	}
-	
+
 	@Test
 	public void testExportEmpty() {
+		final String file = FILE_PATH + fileNum;
+
 		IDesignDoc testDoc = new HBDesignDoc(DESIGN_NAME);
-		
+		test.doExport(file, testDoc);
+
+		String exported = ExportUtil.readAll(file, true);
+		String expected = String.format(
+				"" + "{" + "\"name\":\"%s\"," + "\"interfaces\": [" + "],"
+						+ "\"classes\": [" + "]" + "}", DESIGN_NAME)
+				.replaceAll("\\s*", "");
+
+		Assert.assertEquals(
+				"JsonExporter should export correctly on empty designs",
+				expected, exported);
 	}
-	
+
 	@After
 	public void tearDown() {
 		test = null;
--- test/test/edu/iastate/cs362/hb/util/ExportUtil.java
+++ test/test/edu/iastate/cs362/hb/util/ExportUtil.java
@@ -2,6 +2,18 @@ package test.edu.iastate.cs362.hb.util;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.IO;
+
+import edu.iastate.cs362.hb.model.IDesignDoc;
+import edu.iastate.cs362.hb.model.IObject;
+import edu.iastate.cs362.hb.model.IRelationship;
+import edu.iastate.cs362.hb.model.impl.HBClass;
+import edu.iastate.cs362.hb.model.tree.IHBTreeVisitor;
+import edu.iastate.cs362.hb.model.tree.Pair;
 
 /**
  * Utility class used for testing the exporters
@@ -9,6 +21,11 @@ import java.io.FileReader;
  *
  */
 public class ExportUtil {
+	
+	// Format strings used for build
+	public static final String JSON_FORMAT = "JSON";
+	public static final String XML_FORMAT = "XML";
+	public static final String SRC_FORMAT = "SOURCE";
 	
 	/**
 	 * Checks whether the src string contains all of the given strings to 
@@ -57,5 +74,83 @@ public class ExportUtil {
 		} catch (Exception e) {
 			throw new RuntimeException(String.format("Unable to read file: %s", filePath), e);
 		}
+	}
+	
+	/**
+	 * Returns the string that is the properly exported string for the given
+	 * design doc.
+	 * @param d
+	 * @param format
+	 * 		Either JSON, XML, or SOURCE 
+	 * @return
+	 */
+	public static String buildProperExportString(IDesignDoc d, String format) {
+		throw new RuntimeException("Unimplemented");
+//		format = format.toUpperCase();
+//		if (format.equals(JSON_FORMAT)) {
+//			return buildJsonExportString(d);
+//		} else if (format.equals(XML_FORMAT)) {
+//			
+//		} else if (format.equals(SRC_FORMAT)) {
+//			
+//		}
+//		
+//		throw new RuntimeException("Unknown export format: " + format);
+	}
+	
+	private static String buildJsonExportString(IDesignDoc d) {
+		// TODO shelved. Idk if there's any point since I'm basically just testing
+		// the Gson library
+		throw new RuntimeException("Unimplemented");
+//		StringBuilder sb = new StringBuilder();
+//		String output = ""
+//				+ "{"
+//				+ "\"name\":\"%s\","
+//				+ "\"interfaces\":%s,"
+//				+ "\"classes\":%s"
+//				+ "}";
+//		
+//		final List<IObject> classes = new ArrayList<>();
+//		final List<IObject> interf = new ArrayList<>();
+//		
+//		getAllObjs(d, classes, interf);
+//		
+//		return "";
+	}
+	
+	// Puts all of the interfaces and classes into the given lists
+	private static void getAllObjs(IDesignDoc d, final List<IObject> classes, final List<IObject> interf) {
+		// TODO shelved. Idk if there's any point since I'm basically just testing
+		// the Gson library
+		d.traverse(new IHBTreeVisitor() {
+			@Override
+			public void visit(IObject o, Set<Pair<IRelationship, IObject>> superTypes) {
+				if (o.getClass() == HBClass.class) {
+					classes.add(o);
+				} else {
+					interf.add(o);
+				}
+			}
+		});
+	}
+	
+	private static String buildJsonClassesString(final List<IObject> classes) {
+		// TODO shelved. Idk if there's any point since I'm basically just testing
+		// the Gson library
+		throw new RuntimeException("Unimplemented");
+//		String classFormatStr = ""
+//				+ "{"
+//				+ "\"fields\":%s" // Build a fields string as well
+//				+ "\"id\":%d"
+//				+ "\"pkg\":%s"
+//				+ "\"name\":%s"
+//				+ "\"modifiers\":" // Build a modifiers string
+//				+ "\"methods\":%s"; // Build a methods string
+//		String output = "[" + classFormatStr + "]";
+//		for (int i = 0; i < classes.size(); ++i) {
+//			
+//		}
+//		
+//		return "";
 	}
 }
