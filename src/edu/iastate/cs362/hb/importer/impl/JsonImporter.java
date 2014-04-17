47b5ce338669de10d18f2165ad6bf9eed9b141b3
Brandon <bmaxwell921@gmail.com>
2014-04-17 17:33:04 -0500

    TWO LINES

--- src/edu/iastate/cs362/hb/importer/impl/JsonImporter.java
+++ src/edu/iastate/cs362/hb/importer/impl/JsonImporter.java
@@ -1,17 +1,16 @@
 package edu.iastate.cs362.hb.importer.impl;
 
+import com.google.gson.Gson;
+
 import edu.iastate.cs362.hb.importer.IImporter;
 import edu.iastate.cs362.hb.model.IDesignDoc;
 
 public class JsonImporter implements IImporter {
 
-	public JsonImporter() {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
 	public IDesignDoc doImport(String path) {
-		// TODO Auto-generated method stub
+		Gson gson = new Gson();
+		
 		return null;
 	}
 
