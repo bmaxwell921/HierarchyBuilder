8d58465abb6097f4ff001c25092a0220041660b4
Brandon <bmaxwell921@gmail.com>
2014-04-21 15:28:06 -0500

    Changed throws clause to just Exception and added to javadoc. Minified parameter names

--- src/edu/iastate/cs362/hb/model/tree/ITree.java
+++ src/edu/iastate/cs362/hb/model/tree/ITree.java
@@ -7,7 +7,6 @@ import edu.iastate.cs362.hb.exceptions.HBDuplicateRelationshipException;
 import edu.iastate.cs362.hb.exceptions.HBMultipleObjectsFoundException;
 import edu.iastate.cs362.hb.exceptions.HBObjectNotFoundException;
 import edu.iastate.cs362.hb.exceptions.HBRelationshipNotFoundException;
-import edu.iastate.cs362.hb.model.IManager;
 import edu.iastate.cs362.hb.model.IObject;
 import edu.iastate.cs362.hb.model.IObjectBox;
 import edu.iastate.cs362.hb.model.IRelationship;
@@ -23,70 +22,82 @@ public interface ITree {
 	/**
 	 * Method used to get an object from the tree
 	 * 
-	 * @param className
+	 * @param objName
 	 * @return
 	 * @throws HBObjectNotFoundException
-	 * @throws HBMultipleObjectsFoundException 
+	 *             if the object isn't found in the tree
+	 * @throws HBMultipleObjectsFoundException
+	 *             if multiple objects exist in the tree with this name
 	 */
-	public IObject getObject(String className) throws HBObjectNotFoundException, HBMultipleObjectsFoundException;
+	public IObject getObject(String objName) throws Exception;
 
 	/**
 	 * Method used to add an object to the tree
 	 * 
-	 * @param newClass
+	 * @param newObj
 	 * @return
 	 * @throws HBDuplicateObjectFoundException
 	 *             thrown if an object already exists in the tree with the given
 	 *             name and package
 	 */
-	public boolean addObject(IObject newClass)
-			throws HBDuplicateObjectFoundException;
+	public boolean addObject(IObject newObj) throws Exception;
 
 	/**
 	 * Adds the given relationship between the two class
 	 * 
-	 * @param fromClass
-	 * @param toClass
-	 * @param relationship
+	 * @param from
+	 * @param to
+	 * @param rel
 	 * @return
+	 * @throws HBObjectNotFoundException
+	 *             If either fromClass or toClass don't exist in the tree
+	 * @throws HBDuplicateRelationshipException
+	 *             If the relationship fromClass to toClass already exists
 	 */
-	public boolean addRelationship(IObject fromClass, IObject toClass,
-			IRelationship relationship) throws HBObjectNotFoundException,
-			HBDuplicateRelationshipException;
+	public boolean addRelationship(IObject from, IObject to, IRelationship rel)
+			throws Exception;
 
 	/**
 	 * Removes the given relationship from the two classes
 	 * 
-	 * @param fromClass
-	 * @param toClass
+	 * @param from
+	 * @param to
 	 * @return
+	 * @throws HBObjectNotFoundException
+	 *             If either given class doesn't exist in the tree
+	 * @throws HBRelationshipNotFoundException
+	 *             If the relationship doesn't exist
 	 */
-	public boolean removeRelationship(IObject fromClass, IObject toClass)
-			throws HBObjectNotFoundException, HBRelationshipNotFoundException;
+	public boolean removeRelationship(IObject from, IObject to)
+			throws Exception;
 
 	/**
 	 * Method used to remove an object from the tree.
 	 * 
 	 * Side effect: Any relationships to or from this object are removed
 	 * 
-	 * @param toRemove
+	 * @param rem
 	 * @return
+	 * @throws HBObjectNotFoundException
+	 *             If the given object doesn't exist in the tree
 	 */
-	public boolean removeObject(IObject toRemove)
-			throws HBObjectNotFoundException;
-	
+	public boolean removeObject(IObject rem) throws HBObjectNotFoundException;
+
 	/**
-	 * Method used to get a list of all the objects that match
-	 * the given name
+	 * Method used to get a list of all the objects that match the given name.
+	 * 
+	 * TODO remove after we implement Id Map
+	 * 
 	 * @param name
 	 * @return
 	 */
 	public List<IObjectBox> getMatchingObjects(String name);
-	
+
 	/**
-	 * Traverses the tree hierarchy with the given visitor. Starts 
-	 * by going through the roots in alphabetical order and following
-	 * their classes all the way down
+	 * Traverses the tree hierarchy with the given visitor. Starts by going
+	 * through the roots in alphabetical order and following their classes all
+	 * the way down
+	 * 
 	 * @param visitor
 	 */
 	public void traverse(IHBTreeVisitor visitor);
