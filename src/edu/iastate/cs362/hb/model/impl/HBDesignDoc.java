c1f96206cbdb580abfad85d8fdd308bb0cdd72e9
Brandon <bmaxwell921@gmail.com>
2014-04-21 16:09:48 -0500

    Minifying code and changing to match interface

--- src/edu/iastate/cs362/hb/model/impl/HBDesignDoc.java
+++ src/edu/iastate/cs362/hb/model/impl/HBDesignDoc.java
@@ -38,9 +38,8 @@ public class HBDesignDoc implements IDesignDoc {
 	}
 
 	@Override
-	public boolean addInstanceField(String className, String instanceFieldName,
-			String... modifiers) throws HBClassNotFoundException,
-			HBObjectNotFoundException, HBMultipleObjectsFoundException {
+	public boolean addInstanceField(String className, String fieldName,
+			String... modifiers) throws Exception {
 		IObject obj = tree.getObject(className);
 		// if obj is not a Class (its an interface) then we can't add an
 		// instance field
@@ -50,101 +49,88 @@ public class HBDesignDoc implements IDesignDoc {
 		// now we know obj is an HBClass.
 		HBClass cl = (HBClass) obj;
 
-		IInstanceField i = new HBInstanceField(instanceFieldName);
+		IInstanceField i = new HBInstanceField(fieldName);
 		i.addModifiers(modifiers);
 
 		return cl.addInstanceField(i);
 	}
 
 	@Override
-	public boolean addRelationship(String fromClass, String toClass,
-			String relationship) throws HBObjectNotFoundException,
-			HBDuplicateRelationshipException, HBMultipleObjectsFoundException {
-		IObject from = tree.getObject(fromClass);
-		IObject to = tree.getObject(toClass);
-		HBRelationship r = new HBRelationship(relationship);
+	public boolean addRelationship(String fromName, String toName, String rel)
+			throws Exception {
+		IObject from = tree.getObject(fromName);
+		IObject to = tree.getObject(toName);
+		HBRelationship r = new HBRelationship(rel);
 		return tree.addRelationship(from, to, r);
 	}
 
 	@Override
-	public boolean addPackage(String packageName, String className)
-			throws HBObjectNotFoundException, HBMultipleObjectsFoundException {
-		IObject clazz = tree.getObject(className);
-		clazz.addPackage(packageName);
+	public boolean addPackage(String pkgName, String objName) throws Exception {
+		IObject clazz = tree.getObject(objName);
+		clazz.addPackage(pkgName);
 		return true;
 	}
 
 	@Override
-	public boolean addInstanceMethod(String className, String methodName,
-			String params, String... modifiers)
-			throws HBObjectNotFoundException, MalformattedCommandException,
-			HBDuplicateMethodException, HBMultipleObjectsFoundException {
-		return addMethod(className, methodName, params, modifiers);
+	public boolean addInstanceMethod(String objName, String methodName,
+			String params, String... modifiers) throws Exception {
+		return addMethod(objName, methodName, params, modifiers);
 	}
 
 	@Override
-	public boolean addStaticMethod(String className, String methodName,
-			String params, String... modifiers)
-			throws MalformattedCommandException, HBObjectNotFoundException,
-			HBDuplicateMethodException, HBMultipleObjectsFoundException {
-		return addMethod(className, methodName, params, modifiers);
+	public boolean addStaticMethod(String objName, String methodName,
+			String params, String... modifiers) throws Exception {
+		return addMethod(objName, methodName, params, modifiers);
 	}
 
-	private boolean addMethod(String className, String methodName,
-			String params, String... modifiers)
-			throws MalformattedCommandException, HBObjectNotFoundException,
-			HBDuplicateMethodException, HBMultipleObjectsFoundException {
+	private boolean addMethod(String objName, String methodName, String params,
+			String... modifiers) throws Exception {
 		IMethod method = new HBMethod(methodName);
 		method.addModifiers(modifiers);
 		method.addArguments(params);
 
-		IObject clazz = tree.getObject(className);
+		IObject clazz = tree.getObject(objName);
 		return clazz.addMethod(method);
 	}
 
 	@Override
-	public boolean removeMethod(String className, String methodName)
-			throws HBObjectNotFoundException, HBMultipleObjectsFoundException,
-			HBMethodNotFoundException {
-		HBClass clazz = (HBClass) tree.getObject(className);
+	public boolean removeMethod(String objName, String methodName)
+			throws Exception {
+		HBClass clazz = (HBClass) tree.getObject(objName);
 		IMethod method = clazz.getMethod(methodName);
 		clazz.removeMethod(method);
 		return true;
 	}
 
 	@Override
-	public boolean removePackage(String className)
-			throws HBObjectNotFoundException, HBMultipleObjectsFoundException {
-		IObject toRem = tree.getObject(className);
+	public boolean removePackage(String objName) throws Exception {
+		IObject toRem = tree.getObject(objName);
 		return toRem.removePackage();
 	}
 
 	@Override
-	public boolean removeRelationship(String fromClass, String toClass)
-			throws HBMultipleObjectsFoundException, HBObjectNotFoundException,
-			HBRelationshipNotFoundException {
-		IObject from = tree.getObject(fromClass);
-		IObject to = tree.getObject(toClass);
+	public boolean removeRelationship(String fromName, String toName)
+			throws Exception {
+		IObject from = tree.getObject(fromName);
+		IObject to = tree.getObject(toName);
 		return tree.removeRelationship(from, to);
 	}
 
 	@Override
-	public boolean removeClass(String toRemove)
-			throws HBObjectNotFoundException, HBMultipleObjectsFoundException {
-		IObject toRem = tree.getObject(toRemove);
+	public boolean removeObj(String rem) throws Exception,
+			HBMultipleObjectsFoundException {
+		IObject toRem = tree.getObject(rem);
 		return tree.removeObject(toRem);
 	}
 
 	@Override
-	public boolean createClass(String name)
-			throws HBDuplicateObjectFoundException {
+	public boolean createClass(String name) throws Exception {
 		IClass clazz = new HBClass(name);
 		return tree.addObject(clazz);
 	}
 
 	@Override
-	public boolean createInterface(String name)
-			throws HBDuplicateObjectFoundException {
+	public boolean createInterface(String name) throws Exception {
 		IObject interf = new HBInterface(name);
 		return tree.addObject(interf);
 	}
@@ -182,28 +168,26 @@ public class HBDesignDoc implements IDesignDoc {
 	}
 
 	@Override
-	public String listObject(String name) throws HBObjectNotFoundException,
-			HBMultipleObjectsFoundException {
+	public String listObject(String name) throws Exception {
 		return tree.getObject(name).list();
 	}
 
 	@Override
-	public boolean changeName(String name, String newName)
-			throws HBObjectNotFoundException, HBMultipleObjectsFoundException {
+	public boolean changeName(String name, String newName) throws Exception {
 		return tree.getObject(name).changeName(newName);
 	}
 
 	@Override
-	public boolean changePackage(String name, String packageName)
-			throws HBObjectNotFoundException, HBMultipleObjectsFoundException {
-		return tree.getObject(name).changePackage(packageName);
+	public boolean changePackage(String name, String pkgName)
+			throws Exception {
+		return tree.getObject(name).changePackage(pkgName);
 	}
 
 	@Override
 	public void traverse(IHBTreeVisitor visitor) {
 		tree.traverse(visitor);
 	}
-	
+
 	@Override
 	public void addAllInner(Set<? extends IObject> interfaces,
 			Set<? extends IObject> classes) {
@@ -219,14 +203,13 @@ public class HBDesignDoc implements IDesignDoc {
 					tree.addObject(clazz);
 				}
 			}
-			
-			/* 
-			 * Then add the relationships
-			 * TODO how to do this?? I think I'll need to rework the
-			 * IObject class to have relationships in it, otherwise
-			 * Gson won't export that information
-			 */ 
-			
+
+			/*
+			 * Then add the relationships TODO how to do this?? I think I'll
+			 * need to rework the IObject class to have relationships in it,
+			 * otherwise Gson won't export that information
+			 */
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
