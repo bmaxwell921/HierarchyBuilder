a9fd9c73c882cf198afaeb0374f9e00fe3b2fdde
Brandon <bmaxwell921@gmail.com>
2014-04-22 10:24:18 -0500

    Class to replace IInstanceField and IArgument

---
+++ src/edu/iastate/cs362/hb/model/IVariable.java
@@ -0,0 +1,15 @@
+package edu.iastate.cs362.hb.model;
+
+import edu.iastate.cs362.hb.model.attributes.Identifiable;
+import edu.iastate.cs362.hb.model.attributes.Modifiable;
+import edu.iastate.cs362.hb.model.attributes.Nameable;
+
+public interface IVariable extends Nameable, Modifiable, Identifiable {
+	
+	/**
+	 * Returns the type of the variable
+	 * @return
+	 */
+	public String getType();
+
+}
