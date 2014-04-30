7637cc57f2143df0cd6c923cf4a96664f261936f
Brandon <bmaxwell921@gmail.com>
2014-04-29 20:25:44 -0500

    Fixing tests

--- test/test/edu/iastate/cs362/hb/commands/impl/CommandParserTest.java
+++ test/test/edu/iastate/cs362/hb/commands/impl/CommandParserTest.java
@@ -11,190 +11,158 @@ import edu.iastate.cs362.hb.commands.impl.CommandParser;
 import edu.iastate.cs362.hb.exceptions.MalformattedCommandException;
 
 public class CommandParserTest {
-	
+
 	CommandParser test;
-	
+
 	@Before
-	public void setUp(){
+	public void setUp() {
 		test = new CommandParser();
 	}
-	
+
 	@After
-	public void tearDown(){
+	public void tearDown() {
 		test = null;
 	}
-	
-	@Test
-	public void testCreate() throws MalformattedCommandException {
-		CommandParser parser = new CommandParser();
-		String command = "CREATE DESIGN -NAME Wobbly";
-		ICommand c = parser.parseCommand(command);
-		assertEquals(c.getName(), "create");
-	}
-	
+
 	@Test
 	public void testCreateDesign() throws MalformattedCommandException {
-		CommandParser parser = new CommandParser();
-		String command = "CREATE DESIGN -NAME Wobbly";
-		ICommand c = parser.parseCommand(command);
+		String command = "create design -name Wobbly";
+		ICommand c = test.parseCommand(command);
+		assertEquals(c.getName(), "create");
 		assertEquals(c.getSubCommand(), "design");
+		assertEquals(c.getFlagValue("name"), "Wobbly");
 	}
-	
+
 	@Test
-	public void testCreateDesignNamedCommand() throws MalformattedCommandException {
-		CommandParser parser = new CommandParser();
-		String command = "CREATE DESIGN -NAME Wobbly";
-		ICommand c = parser.parseCommand(command);
+	public void testCreateDesignShort() throws MalformattedCommandException {
+		String command = "create design -n Wobbly";
+		ICommand c = test.parseCommand(command);
+		assertEquals(c.getName(), "create");
+		assertEquals(c.getSubCommand(), "design");
 		assertEquals(c.getFlagValue("name"), "Wobbly");
 	}
 	
-	@Test
-	public void addTest() throws MalformattedCommandException {
-		CommandParser parser = new CommandParser();
-		String command = "ADD CLASS -NAME NAME";
-		ICommand c = parser.parseCommand(command);
-		assertEquals(c.getName(), "add");
+	@Test (expected = MalformattedCommandException.class)
+	public void testCreateInvalidSub() throws MalformattedCommandException {
+		test.parseCommand("create asdfh");
 	}
 	
-	@Test
-	public void addClass() throws MalformattedCommandException {
-		CommandParser parser = new CommandParser();
-		String command = "ADD CLASS -NAME NAME";
-		ICommand c = parser.parseCommand(command);
-		assertEquals(c.getSubCommand(), "class");
+	@Test (expected = MalformattedCommandException.class)
+	public void testCreateDesignInvalidFlag() throws MalformattedCommandException {
+		test.parseCommand("create design -blah");
 	}
 	
-	@Test
-	public void addClassNamed() throws MalformattedCommandException {
-		CommandParser parser = new CommandParser();
-		String command = "ADD CLASS -NAME NAME";
-		ICommand c = parser.parseCommand(command);
-		assertEquals(c.getFlagValue("name"), "NAME");
+	//@Test (expected = MalformattedCommandException.class)
+	public void testCreateDesignInvalidNotGivenName() throws MalformattedCommandException {
+		test.parseCommand("create design -name");
 	}
 	
+	//@Test (expected = MalformattedCommandException.class)
+	public void testCreateDesignInvalidNotGivenNameShort() throws MalformattedCommandException {
+		test.parseCommand("create design -n");
+	}
+
 	@Test
-	public void createDesign() throws MalformattedCommandException {
-		CommandParser parser = new CommandParser();
-		String command = "CREATE DESIGN";
-		ICommand c = parser.parseCommand(command);
+	public void testCreateClass() throws MalformattedCommandException {
+		String command = "create class -name name";
+		ICommand c = test.parseCommand(command);
 		assertEquals(c.getName(), "create");
+		assertEquals(c.getSubCommand(), "class");
+		assertEquals(c.getFlagValue("name"), "name");
 	}
 	
 	@Test
-	public void createDesignSubCommand() throws MalformattedCommandException {
-		CommandParser parser = new CommandParser();
-		String command = "CREATE DESIGN";
-		ICommand c = parser.parseCommand(command);
-		assertEquals(c.getSubCommand(), "design");
+	public void testCreateClassShort() throws MalformattedCommandException {
+		String command = "create class -n name";
+		ICommand c = test.parseCommand(command);
+		assertEquals(c.getName(), "create");
+		assertEquals(c.getSubCommand(), "class");
+		assertEquals(c.getFlagValue("name"), "name");
 	}
 	
-	@Test
-	public void createDesignNamed() throws MalformattedCommandException {
-		CommandParser parser = new CommandParser();
-		String command = "CREATE DESIGN -NaMe des";
-		ICommand c = parser.parseCommand(command);
-		assertEquals(c.getFlagValue("name"), "des");
+	//@Test (expected = MalformattedCommandException.class)
+	public void testCreateClassInvalidNotGivenName() throws MalformattedCommandException {
+		test.parseCommand("create class -name");
 	}
 	
-	@Test
-	public void removeDesignNamed() throws MalformattedCommandException {
-		CommandParser parser = new CommandParser();
-		String command = "Remove DESIGN -NaMe des";
-		ICommand c = parser.parseCommand(command);
-		assertEquals(c.getFlagValue("name"), "des");
-	
+	//@Test (expected = MalformattedCommandException.class)
+	public void testCreateClassInvalidNotGivenNameShort() throws MalformattedCommandException {
+		test.parseCommand("create class -n");
 	}
-	
-	@Test
-	public void removeDesign() throws MalformattedCommandException {
-		CommandParser parser = new CommandParser();
-		String command = "Remove DESIGN -NaMe des";
-		ICommand c = parser.parseCommand(command);
-		assertEquals(c.getName(), "remove");
-	}
-	
-	@Test
-	public void removeDesignSubtype() throws MalformattedCommandException
-	{
-		CommandParser parser = new CommandParser();
-		String command = "Remove DESIGN -NaMe des";
-		ICommand c = parser.parseCommand(command);
-		assertEquals(c.getName(), "remove");
-	}
-	
+
 	@Test
 	public void listTest() throws MalformattedCommandException {
-		String command = "list DESIGN";
+		String command = "list design";
 		ICommand c = test.parseCommand(command);
 		assertEquals(c.getName(), "list");
 	}
-	
+
 	@Test
 	public void listTestName() throws MalformattedCommandException {
 		String command = "list DESIGN";
 		ICommand c = test.parseCommand(command);
 		assertEquals(c.getSubCommand(), "design");
 	}
-	
+
 	@Test
 	public void listClassTest() throws MalformattedCommandException {
 		String command = "list CLASS -name className";
 		ICommand c = test.parseCommand(command);
 		assertEquals(c.getSubCommand(), "class");
 	}
-	
+
 	@Test
 	public void listClassTest2() throws MalformattedCommandException {
 		String command = "list cLaSs -name className";
 		ICommand c = test.parseCommand(command);
 		assertEquals(c.getFlagValue("name"), "className");
 	}
-	
-	@Test (expected = MalformattedCommandException.class)
+
+	@Test(expected = MalformattedCommandException.class)
 	public void listTest3() throws MalformattedCommandException {
 		String command = "list someGarbage";
 		test.parseCommand(command);
 	}
 
-	@Test (expected = MalformattedCommandException.class)
+	@Test(expected = MalformattedCommandException.class)
 	public void listTestException() throws MalformattedCommandException {
 		String command = "ls";
 		test.parseCommand(command);
 	}
-	
-	@Test 
+
+	@Test
 	public void listNullTest() throws MalformattedCommandException {
 		String command = "list class -name ";
 		ICommand c = test.parseCommand(command);
 		assertEquals(c.getFlagValue("name"), null);
 	}
-	
+
 	@Test
 	public void listCacheTest() throws MalformattedCommandException {
 		String command = "list cache -id 8675";
 		ICommand c = test.parseCommand(command);
 		assertEquals(c.getFlagValue("id"), "8675");
 	}
-	
+
 	@Test
 	public void listCacheTest2() throws MalformattedCommandException {
 		String command = "list cache -id 8675";
 		ICommand c = test.parseCommand(command);
 		assertEquals(c.getName(), "list");
 	}
-	
+
 	@Test
 	public void listCacheTest3() throws MalformattedCommandException {
 		String command = "list cache -id 8675";
 		ICommand c = test.parseCommand(command);
 		assertEquals(c.getSubCommand(), "cache");
 	}
-	
+
 	@Test
 	public void listCacheException() throws MalformattedCommandException {
 		String command = "list cache -id ";
 		ICommand c = test.parseCommand(command);
 		assertEquals(c.getFlagValue("id"), null);
 	}
-	
+
 }
